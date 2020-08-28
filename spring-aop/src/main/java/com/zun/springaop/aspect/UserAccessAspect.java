48bc393222bfa6a0c269ff83ad2a0c54a5ba3d8f
Zun <zunsamuel97@gmail.com>
2020-08-28 01:09:51 -0500

    step 5

--- spring-aop/src/main/java/com/zun/springaop/aspect/UserAccessAspect.java
+++ spring-aop/src/main/java/com/zun/springaop/aspect/UserAccessAspect.java
@@ -1,8 +1,7 @@
 package com.zun.springaop.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +12,29 @@ public class UserAccessAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* com.zun.springaop.business.*.*(..))")
-    public void before(JoinPoint joinPoint){
+    @Before("execution(* com.zun.springaop.data.*.*(..))")
+    public void before(JoinPoint joinPoint) {
         logger.info("Check for user access");
         logger.info("Allow execution for {}", joinPoint);
+    }
+
+    @AfterReturning(
+            value = "execution(* com.zun.springaop.business.*.*(..))",
+            returning = "res")
+    public void afterReturning(JoinPoint joinPoint, Object res) {
+        logger.info("{} returned with value {}", joinPoint, res);
+    }
+
+    @AfterThrowing(
+            value = "execution(* com.zun.springaop.business.*.*(..))",
+            throwing = "exception")
+    public void afterThrowing(JoinPoint joinPoint, Object exception) {
+        logger.info("{} throws exception {}", joinPoint, exception);
+    }
+
+    @After("execution(* com.zun.springaop.business.*.*(..))")
+    public void after(JoinPoint joinPoint) {
+        logger.info("after execution of {}", joinPoint);
     }
 
 }
