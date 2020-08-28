89b3579a2e0c5a90eb4f46dc0e754bfc5d4abe0b
Zun <zunsamuel97@gmail.com>
2020-08-27 23:53:36 -0500

    step 5

--- spring-in-10-steps/src/main/java/com/zun/springin10steps/SpringIn10StepsApplication.java
+++ spring-in-10-steps/src/main/java/com/zun/springin10steps/SpringIn10StepsApplication.java
@@ -2,12 +2,19 @@ package com.zun.springin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIn10StepsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn10StepsApplication.class, args);
+
+		Arrays.stream(
+				applicationContext.getBeanDefinitionNames()
+		).forEach(System.out::println);
 	}
 
 }
