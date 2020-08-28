3e43d6bb8c305c0b81526e08ceddea5a7e509661
Zun <zunsamuel97@gmail.com>
2020-08-27 19:01:44 -0500

    step 17

--- basics/src/main/java/com/zun/basics/basic/BinarySearchImpl.java
+++ basics/src/main/java/com/zun/basics/basic/BinarySearchImpl.java
@@ -1,14 +1,21 @@
 package com.zun.basics.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("bubble")
@@ -20,5 +27,15 @@ public class BinarySearchImpl {
         System.out.println(sortAlgorithm.getClass().getSimpleName());
 
         return 3;
+    }
+
+    @PostConstruct
+    public void postCounstruct(){
+        LOGGER.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        LOGGER.info("preDestroy");
     }
 }
