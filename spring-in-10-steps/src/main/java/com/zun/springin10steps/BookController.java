dabfd6bce881582a2002378c33fe3fc8c3c3c03b
Zun <zunsamuel97@gmail.com>
2020-08-27 23:36:01 -0500

    step 4

---
+++ spring-in-10-steps/src/main/java/com/zun/springin10steps/BookController.java
@@ -0,0 +1,19 @@
+package com.zun.springin10steps;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks(){
+        return Arrays.asList(
+                new Book(1, "Mastering Spring 5.0", "Ranga Karana")
+        );
+    }
+
+}
